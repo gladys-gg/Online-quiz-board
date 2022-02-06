edf1bc0b4d4b737176ffe487dd2e6af636b753dd
Gladys Mwangi <gladysmwangi04@yahoo.com>
2022-02-06 07:07:19 +0300

    script.js: defined the submit button

--- js/script.js
+++ js/script.js
@@ -3,3 +3,5 @@ const question2Answer = document.getElementById("que2answer4");
 const question3Answer = document.getElementById("que3answer1");
 const question4Answer = document.getElementById("que4answer4");
 const question5Answer = document.getElementById("que5answer1");
+
+const submit = document.getElementById("submit");
