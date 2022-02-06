0b2616e62ead43d6ecfe949abe4071aa0296490a
Gladys Mwangi <gladysmwangi04@yahoo.com>
2022-02-06 07:05:20 +0300

    js: created a folder that holds script.js and defined the answers to the question in it

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const question1Answer = document.getElementById("que1answer1");
+const question2Answer = document.getElementById("que2answer4");
+const question3Answer = document.getElementById("que3answer1");
+const question4Answer = document.getElementById("que4answer4");
+const question5Answer = document.getElementById("que5answer1");
